4d5a9309ac4d786ddde48fe43ff1e8a1dc37d798
Akshaj Devanathan <Flower.ssv@gmail.com>
2023-02-24 19:49:12 -0600

    login part done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,38 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 export default function Login() {
-   let correctdata=[
-    {
-        email:"Akshaj@gmail.com",
-        password:"Akshaj123"
-    },
-    {
-        email:"Codeyoung@gmail.com",
-        password:"Codeyoung123"
-    }
-   ] 
+    const navigate=useNavigate()
+    let correctdata = [
+        {
+            email: "Akshaj@gmail.com",
+            password: "Akshaj123"
+        },
+        {
+            email: "Codeyoung@gmail.com",
+            password: "Codeyoung123"
+        }
+    ]
     const { register, handleSubmit, formState: { errors } } = useForm()
     let submit_data = (data) => {
-        correctdata.forEach(obj=>{
-            if(obj.email?.toLowerCase())
+        let emailflag = 0
+        let passwordflag = 0
+        correctdata.forEach(obj => {
+            if (obj.email?.toLowerCase() == data.email.toLowerCase()) {
+                emailflag=1
+                if (obj.password==data.password){
+                    passwordflag=1
+                }
+            }
         })
+        if(emailflag && passwordflag){
+            navigate("dashboard")
+        }
+        else{
+            alert("invalid credentials")
+        }
+        
     }
     return (
         <div>
@@ -26,7 +42,7 @@ export default function Login() {
                 <input type="password" placeholder='Password' className='form-control' {...register('password', { required: true })} />
                 {errors.password && <span className='text-danger'>This field is required</span>}
                 <br></br>
-                <button type='submit' className='btn btn-success'>Submit</button>
+                <button type='submit' className='btn btn-success'>Login</button>
             </form>
             <p className='lead'>A new user?</p>
             <Link className='btn btn-info' to="signup" >Signup</Link>
